d516799c26152a8e0007908d83fd756b0a37c3fc
fydx <lbgg918@gmail.com>
2013-04-27 21:59:23 +0800

    server 返回字符串

--- src/com/aibang/open/client/demo/JavaDemo.java
+++ src/com/aibang/open/client/demo/JavaDemo.java
@@ -31,22 +31,21 @@ import org.json.JSONObject;
  * </pre>
  */
 public class JavaDemo {
-/*	private String jsonString;
-	private JSONObject jsonObject;
-	private int place_num;*/
+	/*
+	 * private String jsonString; private JSONObject jsonObject; private int
+	 * place_num;
+	 */
 	private List<place> places;
+
 	public static void main(String[] args) throws AibangException {
 		JavaDemo demo = new JavaDemo();
 		demo.printUsage();
-	/*	if (args.length == 0) {
-			demo.printUsage();
-			return;
-		}
-		String[] real_args = new String[args.length - 1];
-		for (int i = 1; i < args.length; i++) {
-			real_args[i - 1] = args[i];
-		}
-		demo.run(args[0], real_args);*/
+		/*
+		 * if (args.length == 0) { demo.printUsage(); return; } String[]
+		 * real_args = new String[args.length - 1]; for (int i = 1; i <
+		 * args.length; i++) { real_args[i - 1] = args[i]; } demo.run(args[0],
+		 * real_args);
+		 */
 	}
 
 	/**
@@ -86,6 +85,7 @@ public class JavaDemo {
 		try {
 			places = place.jsonStringToList("西安", "东大街");
 			System.out.println(places.get(0).toString());
+		
 			server_on();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -94,56 +94,61 @@ public class JavaDemo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	/*	System.out.println("Usage:");
-		System.out.println("search: ");
-		System.out.println("    ./demo search <city> <addr>");
-		System.out.println("biz: ");
-		System.out.println("    ./demo biz <bid>");
-		System.out.println("bus: ");
-		System.out.println("    ./demo bus <city> <start> <end>");
-		System.out.println("post_comment: ");
-		System.out.println("    ./demo post_comment <bid> <score> <content>"); */
-		/*try {
-			jsonString = search("西安", "东大街"); 
-			JSONObject jsonObject_temp = new JSONObject(jsonString);
-			place_num = jsonObject_temp.getInt("total");
-			do {
-				
-			} while (jsonObject_temp.getInt("result_num")>0);
-			showResult(jsonString);
-			try {
-				jsonObject = new JSONObject(jsonString);
-				place_num = jsonObject.getInt("total");
-			//	System.out.println(jsonObject);
-				System.out.println(jsonObject.getString("bizs"));
-				JSONObject jsonObject2 = new JSONObject(jsonObject.getString("bizs"));
-				//System.out.println(jsonObject2.get)
-		    	JSONArray array = jsonObject2.getJSONArray("biz");
-		    	System.out.println(array);
-		    	System.out.println(place.JsonToPlaceList(array).get(0).toString());
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
-		} catch (AibangException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JSONException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}*/
+		/*
+		 * System.out.println("Usage:"); System.out.println("search: ");
+		 * System.out.println("    ./demo search <city> <addr>");
+		 * System.out.println("biz: ");
+		 * System.out.println("    ./demo biz <bid>");
+		 * System.out.println("bus: ");
+		 * System.out.println("    ./demo bus <city> <start> <end>");
+		 * System.out.println("post_comment: ");
+		 * System.out.println("    ./demo post_comment <bid> <score> <content>"
+		 * );
+		 */
+		/*
+		 * try { jsonString = search("西安", "东大街"); JSONObject jsonObject_temp =
+		 * new JSONObject(jsonString); place_num =
+		 * jsonObject_temp.getInt("total"); do {
+		 * 
+		 * } while (jsonObject_temp.getInt("result_num")>0);
+		 * showResult(jsonString); try { jsonObject = new
+		 * JSONObject(jsonString); place_num = jsonObject.getInt("total"); //
+		 * System.out.println(jsonObject);
+		 * System.out.println(jsonObject.getString("bizs")); JSONObject
+		 * jsonObject2 = new JSONObject(jsonObject.getString("bizs"));
+		 * //System.out.println(jsonObject2.get) JSONArray array =
+		 * jsonObject2.getJSONArray("biz"); System.out.println(array);
+		 * System.out.println(place.JsonToPlaceList(array).get(0).toString()); }
+		 * catch (JSONException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 * 
+		 * } catch (AibangException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } catch (JSONException e1) { // TODO
+		 * Auto-generated catch block e1.printStackTrace(); }
+		 */
+	}
+
+	// 得到地点jsonstring 下一步即将此发送到客户端，可以直接转化为jsonarray
+	// 这里暂时仅作为测试所用，发送jsonstring对于客户端解析起来比自己序列话要简单一些
+	private String GetJsonBizsString() throws JSONException, AibangException {
+		String jsonString = search("西安", "西大街");
+		JSONObject jsonObject = new JSONObject(jsonString);
+		String jsonString_bizs = jsonObject.getString("bizs");
+		return jsonString_bizs;
 	}
 
 	private String search(String city, String addr) throws AibangException {
 		return aibang.search(city, addr, null, null, null, null, null, null,
 				null, null);
 	}
-	public String search(String city, String addr,int from) throws AibangException {
+
+	public String search(String city, String addr, int from)
+			throws AibangException {
 		return aibang.search(city, addr, null, null, null, null, null, null,
-				from,null);
+				from, null);
 	}
+
 	private String biz(String bid) throws AibangException {
 		return aibang.biz(bid);
 	}
@@ -172,9 +177,11 @@ public class JavaDemo {
 	private void showResult(String result) {
 		System.out.println(result);
 	}
-	
-	private void server_on()
-	{
+
+	private void server_on() {
+		String line=null;
+		System.out.println("**********************************");
+		System.out.println("**********************************");
 		try {
 			ServerSocket server = null;
 			try {
@@ -190,30 +197,100 @@ public class JavaDemo {
 				// TODO: handle exception
 				System.out.println("Error2");
 			}
-			String line;
+			
 			BufferedReader is = new BufferedReader(new InputStreamReader(
 					socket.getInputStream()));
 			PrintWriter os = new PrintWriter(socket.getOutputStream());
 			BufferedReader sin = new BufferedReader(new InputStreamReader(
 					System.in));
 			System.out.println("Client:" + is.readLine());
-			line = sin.readLine();
-			while (!line.equals("bye")) {
+		    line = new String(GetJsonBizsString());
+		    System.out.println(line); 
+		
 				os.println(line);
 				os.flush();
 				System.out.println("Server" + line);
 				System.out.println("Client:" + is.readLine());
 				line = sin.readLine();
-			}
+		
 			os.close();
 			is.close();
 			socket.close();
 			server.close();
+		
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println("Error3");
 		}
+		
 	}
+
+	private String loadConvert(String theString) {
+		char aChar;
+		int len = theString.length();
+		StringBuffer outBuffer = new StringBuffer(len);
+
+		for (int x = 0; x < len;) {
+			aChar = theString.charAt(x++);
+			if (aChar == '\\') {
+				aChar = theString.charAt(x++);
+				if (aChar == 'u') {
+					// Read the xxxx
+					int value = 0;
+					for (int i = 0; i < 4; i++) {
+						aChar = theString.charAt(x++);
+						switch (aChar) {
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+							value = (value << 4) + aChar - '0';
+							break;
+						case 'a':
+						case 'b':
+						case 'c':
+						case 'd':
+						case 'e':
+						case 'f':
+							value = (value << 4) + 10 + aChar - 'a';
+							break;
+						case 'A':
+						case 'B':
+						case 'C':
+						case 'D':
+						case 'E':
+						case 'F':
+							value = (value << 4) + 10 + aChar - 'A';
+							break;
+						default:
+							throw new IllegalArgumentException(
+									"Malformed \\uxxxx encoding.");
+						}
+					}
+					outBuffer.append((char) value);
+				} else {
+					if (aChar == 't')
+						aChar = '\t';
+					else if (aChar == 'r')
+						aChar = '\r';
+					else if (aChar == 'n')
+						aChar = '\n';
+					else if (aChar == 'f')
+						aChar = '\f';
+					outBuffer.append(aChar);
+				}
+			} else
+				outBuffer.append(aChar);
+		}
+		return outBuffer.toString();
+	}
+
 	private AibangApi aibang;
 	// 这里请使用您自己申请的API KEY
 	private static final String API_KEY = "212d00fbd2799ead7c6be51b067598f2";
--- src/com/aibang/open/client/demo/place.java
+++ src/com/aibang/open/client/demo/place.java
@@ -178,4 +178,5 @@ public class place {
 		return places;
 		
 	}
+	
 }
